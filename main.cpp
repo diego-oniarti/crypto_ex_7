e395d0786e88638051cf7177e29aff5e3228e0e0
diego <diego.oniarti@gmail.com>
2025-11-07 03:17:22 +0100

    save to file

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "utils.h"
 
 #include <cstdint>
+#include <cstdio>
 #include <curl/curl.h>
 #include <filesystem>
+#include <gmp.h>
 #include <iomanip>
 #include <iostream>
 #include <ostream>
@@ -12,8 +14,8 @@ int main(int argc, char *argv[]) {
     if (res) return (int)res;
 
 #ifdef DEBUG
-    std::string cypher_str = "78f9b8444a5ddfaff5734e358917efe50af522065ddf868592c28ea32fe92ee008a6388e37ae5dbc6bf44a83051829c3fb8e37bd617c0b44c2449854b6efa67037ab99b77dd0941dd2be69fb3fd66b0cef6f59310a47331d053b5f379e067e207ba08026362b4ba3aa7ba118a34e63530a54f4d98ec0940a7e5062db0a0d7020";
-    std::string modulo_str = "9e928bc859ee0c34adfa648750f4ee5bd1c4f16b8cd77ec4430e537e1677485bdba49a17c44926d9af76d1dcd97f85149c63b57f978186c7dfff0fe0316203fe318a1bff303a77982ac23280c1cc5cc0f933f1849a433b96310a79aba0a2c6da65647ea45bfbead69ab1f22bf88a358404e8298cdb43188ba623279ce74278e1";
+    std::string cypher_str = "09b9e926d8f0c868723d19e9931ddadadde87caa06e92e832920058369398bddc359e233c08fd50f61ab15ef28f01b81d1b402144f14013c13c141818e993a41744239815b4d71a400937b1ecafb07207c70f2fad2796bbb94bbf9669655e81a17825c9f5c0538d6584b03f131e54822121984acb9fae4bef00b19cc7a2da087";
+    std::string modulo_str = "a093edb98a93189211f232572d3e9204e652690bdedb91641565fe91640410f388fd155c5ac7c00a132b0066bc021d8189249000fe9befeeadc9a91e4806becbecbe5835f71179b43803100586cddf4cbfa9410c1bf7aa0b7a86deb8924659c7b8f1fee25e80ddd97f9524eebc25c1dd73dda168c57c0c92d06b73c2afdb5c09";
 #else
     std::string cypher_str = "2D38AEB156EF11BC165989A12669B30CF20CDA8A196288A2A24262C9B43BD715BA76DBD8C42337D4EC0D7D40A77FE4A5F37A5A59E0E5E5506ABB588225D5F3483F4F4BDE4E3771CEC55F12C0DCCA56F5D9A3110BC50DC47D7D04DB8E4E57044574CA101301C1EFC64A497AF420B286FE6BAF3A4ADC883A2ED24956C8EB502817";
     std::string modulo_str = "9D7113232D0C3E6BE3815FA6BCB431C29D8B38574F522126835734A685A49D7B735E933A1B7B9CF218211265D658D99D9EC2C44E04AABE8997C0D15F52A9F60B2534E8C9F29F50E051403A7FCD8C6D9DB3CD62CD474AD78A24416AF3ACEFFDE9FA17DE1B732D048B608364E2B99569255A525472BAF1EFACCCEFEF705334B4B7";
@@ -42,7 +44,7 @@ int main(int argc, char *argv[]) {
 
         // 2.a
         if (i==1) {
-            s = ceildiv(n, (3*B));
+            s = n/(3*B);
             if (saved_s != -1) s = saved_s;
             while (!checkPKCS(RSA(c,s,e,n))) {
                 s++;
@@ -129,6 +131,14 @@ step2Cend:
         std::cout << t;
     }
     std::cout << std::endl;
+
+    FILE *fout = fopen("result.txt", "w");
+    gmp_fprintf(fout, "%Zd\n", m.get_mpz_t());
+    for (uint8_t t: trimmed) {
+        fprintf(fout, "%c", (char)t);
+    }
+    fprintf(fout, "\n");
+    fclose(fout);
 
     return 0;
 
